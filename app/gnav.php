04b562f7cd30d65e3c25ba5c551c048417c224cd
HisashiImahayashi <yoshithune1192@gmail.com>
2017-12-04 22:58:24 +0900

    「閉じる」リンクは使えないので、非表示

--- app/gnav.php
+++ app/gnav.php
@@ -6,12 +6,14 @@
                 <nav class="navbar navbar-default navbar-fixed-top">
                     <div class="container-fluid">
                         <div class="navbar-header">
+                        <!--
                             <button type="button" class="navbar-toggle collapsed"data-toggle="collapse"data-target="#navbarEexample8">
                                 <span class="sr-only">Toggle navigation</span>
                                 <span class="icon-bar"></span>
                                 <span class="icon-bar"></span>
                                 <span class="icon-bar"></span>
                             </button>
+                            -->
                             <a class="navbar-brand" href="#">
                                 diginovel
                             </a>
